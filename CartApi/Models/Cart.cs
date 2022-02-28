40308209fb881a3497b87417671aaacc4d2df819
Eyu Jia Yi <eyujiayi@MCTs-MacBook-Pro.local>
2022-02-28 13:32:51 +0800

    Initial Commit

---
+++ CartApi/Models/Cart.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace CartApi.Models
+{
+    public class Cart
+    {
+        public int CartId { get; set; }
+
+        public string ProductId { get; set; }
+
+        public double ProductPrice { get; set; }
+
+        public string Total { get; set; }
+
+        public string OrderStatus { get; set; }
+
+        public string OrderId { get; set; }
+    }
+}
