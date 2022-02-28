1f4dc124bf7b668db4a1e2c26b1a47d86917c9bc
Eyu Jia Yi <eyujiayi@MCTs-MacBook-Pro.local>
2022-02-28 16:18:48 +0800

    Fixed

--- CartApi/Models/Cart.cs
+++ CartApi/Models/Cart.cs
@@ -1,18 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace CartApi.Models
 {
     public class Cart
     {
         public int CartId { get; set; }
 
-        public string ProductId { get; set; }
+        public List<Product> Products { get; set; }
 
-        public double ProductPrice { get; set; }
-
-        public string Total { get; set; }
+        public double Total { get; set; }
 
         public string OrderStatus { get; set; }
 
-        public string OrderId { get; set; }
+        public int OrderId { get; set; }
+    }
+    public class Product
+    {
+        public string ProductId { get; set; }
+        public double ProductPrice { get; set; }
     }
 }
